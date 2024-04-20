d2f78d0ac4120e6ec0d9e4df4eb6199322979df2
Nathaniel Rivera <52226518+DeveloperSpoot@users.noreply.github.com>
2024-04-20 03:31:14 -0700

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace CS162_Practice10
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int IdNumber { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public Employee()
+        {
+            Name = "";
+            IdNumber = 0;
+            Department = "";
+            Position = "";
+        }
+
+        public Employee(string name, int idNumber)
+        {
+            Name = name;
+            IdNumber = idNumber;
+        }
+
+        public Employee(string name, int idNumber, string department, string position)
+        {
+            Name = name;
+            IdNumber = idNumber;
+            Department = department;
+            Position = position;
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,29 @@
+namespace CS162_Practice10
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        List<Employee> employees = new List<Employee>();
+
+        private void onForm_Load(object sender, EventArgs e)
+        {
+            employees.Add(new Employee("Susan Meyers", 47899, "Accounting", "Vice President"));
+            employees.Add(new Employee("Mark Jones", 39119, "IT", "Programmer"));
+            employees.Add(new Employee("Joy Rodgers", 8177, "Manufacturing", "Enineer"));
+
+            int index = 1;
+            employees.ForEach(e => {
+                listBox.Items.Add($"Object {index} \n");
+                listBox.Items.Add($"\t Name: {e.Name}");
+                listBox.Items.Add($"\t ID: {e.IdNumber}");
+                listBox.Items.Add($"\t Dept: {e.Department}");
+                listBox.Items.Add($"\t Position: {e.Position}");
+                index += 1;
+            });
+        }
+    }
+}
